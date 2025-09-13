8f201415769165d7f7371bfc10648785e6144f0b
NeonAn <justasneon@gmail.com>
2025-09-13 15:52:17 +0530

    Update start.py

--- Neon/start.py
+++ Neon/start.py
@@ -8,20 +8,6 @@ MY_CUSTOM_LINKS = {
     "Neon": "https://myselfneon.github.io/about/"
 }
 
-START_TEXT = """<b><i>Hey {user}\n\n🔑 I Am {bot}\n🚀 Fast & Reliable Sessions\n🔒 Safe, Secure and Error-Free\n🧩 Your Ultimate STRING Generator !!\n\nCreated By @MyselfNeon 😎</i></b>"""
-
-ABOUT_TEXT = """‣ 📝 𝐌𝐘 𝐃𝐄𝐓𝐀𝐈𝐋𝐒
-• Mʏ Nᴀᴍᴇ : Auto Filter
-• Mʏ Bᴇsᴛ Fʀɪᴇɴᴅ : Tʜɪs Sᴡᴇᴇᴛɪᴇ ❤️
-• Dᴇᴠᴇʟᴏᴘᴇʀ : @MyselfNeon
-• Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ
-• Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ 𝟹
-• DᴀᴛᴀBᴀsᴇ : Mᴏɴɢᴏ DB
-• Bᴏᴛ Sᴇʀᴠᴇʀ : Hᴇʀᴏᴋᴜ
-• Bᴜɪᴌᴅ Sᴛᴀᴛᴜs : ᴠ𝟸.𝟽.𝟷 [Sᴛᴀʙʟᴇ]
-"""
-
-# Start Command
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot: Client, msg: Message):
     args = msg.text.split(maxsplit=1)
@@ -47,36 +33,36 @@ async def start(bot: Client, msg: Message):
                 invite_link = await bot.create_chat_invite_link(int(F_SUB))
             except:
                 await msg.reply("**__Make Sure I'm Admin in Your Channel__**")
-                return
+                return 
             key = InlineKeyboardMarkup(
                 [[
                     InlineKeyboardButton("📢 Uᴘᴅᴀᴛᴇs", url=invite_link.invite_link),
                     InlineKeyboardButton("♻️ Tʀʏ Aɢᴀɪɴ", callback_data="chk")
                 ]]
-            )
+            ) 
             await msg.reply_text(
                 "<i><b><blockquote>🚫 𝐀𝐂𝐂𝐄𝐒𝐒 𝐃𝐄𝐍𝐈𝐄𝐃</blockquote>\n\nJoin My Update Channel To Use Me Once You’ve Joined, Click The Try Again Button To Confirm Your Subscription And Gain Access.\n\nThank You For Staying Updated !!</b></i>",
                 reply_markup=key
             )
-            return
+            return 
 
     me = (await bot.get_me()).mention
-    await msg.reply_text(
-        START_TEXT.format(user=msg.from_user.mention, bot=me),
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""<b><i>Hey {msg.from_user.mention}\n\n🔑 I Am {me}\n🚀 Fast & Reliable Sessions\n🔒 Safe, Secure and Error-Free\n🧩 Your Ultimate STRING Generator !!\n\nCreated By @MyselfNeon 😎</i></b>""",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("⚡ Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ Sᴇssɪᴏɴ ⚡", callback_data="generate")],
+                [InlineKeyboardButton(text="⚡ Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ Sᴇssɪᴏɴ ⚡", callback_data="generate")],
                 [
-                    InlineKeyboardButton("📝 About", callback_data="about"),
+                    InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url="https://t.me/+o1s-8MppL2syYTI9"),
                     InlineKeyboardButton("Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ", url="https://t.me/NeonFiles")
                 ]
             ]
         )
     )
 
-# Try Again / Check Subscription
 @Client.on_callback_query(filters.regex("chk"))
-async def chk(bot: Client, cb: CallbackQuery):
+async def chk(bot : Client, cb : CallbackQuery):
     try:
         await bot.get_chat_member(int(F_SUB), cb.from_user.id)
     except:
@@ -84,60 +70,18 @@ async def chk(bot: Client, cb: CallbackQuery):
             "You Have Joined My Updates Channel. Please Join It And Then Click Try Again 🆘",
             show_alert=True
         )
-        return
-
-    # Edit the message to the start message after successful join
+        return 
     me = (await bot.get_me()).mention
-    await cb.message.edit_text(
-        START_TEXT.format(user=cb.from_user.mention, bot=me),
+    await bot.send_message(
+        chat_id=cb.from_user.id,
+        text=f"""<b><i>Hey {msg.from_user.mention}\n\n🔑 I Am {me}\n🚀 Fast & Reliable Sessions\n🔒 Safe, Secure and Error-Free\n🧩 Your Ultimate STRING Generator !!\n\nCreated By @MyselfNeon 😎</i></b>""",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("⚡ Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ Sᴇssɪᴏɴ ⚡", callback_data="generate")],
+                [InlineKeyboardButton(text="⚡ Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ Sᴇssɪᴏɴ ⚡", callback_data="generate")],
                 [
-                    InlineKeyboardButton("📝 About", callback_data="about"),
+                    InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url="https://t.me/+o1s-8MppL2syYTI9"),
                     InlineKeyboardButton("Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ", url="https://t.me/NeonFiles")
                 ]
             ]
         )
-    )
-    await cb.answer("Access Granted ✅", show_alert=True)
-
-# About Page Callback
-@Client.on_callback_query(filters.regex("about"))
-async def about_page(bot: Client, cb: CallbackQuery):
-    about_buttons = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Support Grp", url="https://t.me/+o1s-8MppL2syYTI9"),
-                InlineKeyboardButton("Source Code", url="https://myselfneon.github.io/about/")
-            ],
-            [
-                InlineKeyboardButton("Home", callback_data="home"),
-                InlineKeyboardButton("Close", callback_data="close")
-            ]
-        ]
-    )
-    await cb.message.edit_text(ABOUT_TEXT, reply_markup=about_buttons)
-
-# Home Callback
-@Client.on_callback_query(filters.regex("home"))
-async def go_home(bot: Client, cb: CallbackQuery):
-    me = (await bot.get_me()).mention
-    await cb.message.edit_text(
-        START_TEXT.format(user=cb.from_user.mention, bot=me),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("⚡ Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ Sᴇssɪᴏɴ ⚡", callback_data="generate")],
-                [
-                    InlineKeyboardButton("📝 About", callback_data="about"),
-                    InlineKeyboardButton("Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ", url="https://t.me/NeonFiles")
-                ]
-            ]
         )
-    )
-
-# Close Callback
-@Client.on_callback_query(filters.regex("close"))
-async def close_message(bot: Client, cb: CallbackQuery):
-    await cb.message.delete()
-    await cb.answer("Closed ✅", show_alert=False)
