a102c433b8973511275e641d01680c3acefddaff
NeonAn <justasneon@gmail.com>
2025-09-20 21:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '|| NeonFiles ||'
+    return """
+    <body style="background-color:black; color:#39FF14; display:flex; justify-content:center; align-items:flex-start; height:100vh; margin:0; font-family:sans-serif; padding-top:20vh; font-size:4rem;">
+        Coded By @MyselfNeon
+    </body>
+    """
 
 if __name__ == "__main__":
     app.run()
