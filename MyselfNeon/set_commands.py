0be76397d943e4af6242c53bf4de11ba7f641dff
NeonAnurag <justasneon@gmail.com>
2025-12-09 14:07:23 +0530

    Update set_commands.py

--- MyselfNeon/set_commands.py
+++ MyselfNeon/set_commands.py
@@ -9,9 +9,9 @@ from pyrogram.types import BotCommand, Message
 
 # --- Edit This List ---
 SET_COMMANDS = [
-    ("start", "Check Alive Status"),
-    ("generate", "Generate Session Strings"),
-    ("broadcast", "Broadcast Msgs to Users")
+    ("start", "𝘊𝘩𝘦𝘤𝘬 𝘈𝘭𝘪𝘷𝘦 𝘚𝘵𝘢𝘵𝘶𝘴"),
+    ("generate", "𝘎𝘦𝘯𝘦𝘳𝘢𝘵𝘦 𝘚𝘦𝘴𝘴𝘪𝘰𝘯 𝘚𝘵𝘳𝘪𝘯𝘨𝘴"),
+    ("broadcast", "𝘉𝘳𝘰𝘢𝘥𝘤𝘢𝘴𝘵 𝘔𝘴𝘨𝘴 𝘵𝘰 𝘜𝘴𝘦𝘳𝘴")
 ]
 
 # --- Internal Command Handler ---
@@ -32,11 +32,12 @@ async def sync_bot_commands(client: Client, message: Message):
         # 04. --- Push to Telegram ---
         await client.set_bot_commands(commands)
         
-        print(f"✓ Commands Synced with Telegram: {SET_COMMANDS}")
+        print(f"✅ Commands Synced with Telegram: {SET_COMMANDS}")
         
         # 05. --- Confirm Success ---
-        await msg.edit_text("***✓ Success! Bot Commands Updated via Plugin.***")
+        await msg.edit_text("**✅ __Success !!\n🎉 Bot Commands Updated via Plugin.__**")
         
     except Exception as e:
         print(f"✗ Failed to Sync Commands: {e}")
-        await msg.edit_text(f"***✗ Error Updating Commands:***\n`{e}`")
+        await msg.edit_text(f"**🚫 __Error Updating Commands:***\n{e}__")
+
